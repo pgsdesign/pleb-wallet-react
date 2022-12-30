e421e8e9e390891ffaf3b6d51027ff7fc66ff055
pgsdesign <online_jjw@me.com>
2022-12-30 14:39:48 -0800

    lesson 5

--- src/App.js
+++ src/App.js
@@ -1,34 +1,103 @@
-import react, {useEffect, useState} from 'react';
-import './App.css';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import Transactions from "./components/Transactions";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [price, setPrice] = useState(0);
+ const [price, setPrice] = useState(null);
+ const [balance, setBalance] = useState(null);
+ const [transactions, setTransactions] = useState([]);
 
-  const getPrice = () => {
-    axios
-      .get("https://api.coinbase.com/v2/prices/BTC-USD/spot")
-      // .then is a promise that will run when the API call is successful
-      .then((res) => {
-        setPrice(res.data.data.amount);
-      })
-      // .catch is a promise that will run if the API call fails
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+ const getPrice = () => {
+   // Axios is a library that makes it easy to make http requests
+   // After we make a request, we can use the .then() method to handle the response asynchronously
+   // This is an alternative to using async/await
+   axios
+     .get("https://api.coinbase.com/v2/prices/BTC-USD/spot")
+     // .then is a promise that will run when the API call is successful
+     .then((res) => {
+      console.log(res.data.data.amount);
+       setPrice(res.data.data.amount);
+     })
+     // .catch is a promise that will run if the API call fails
+     .catch((err) => {
+       console.log(err);
+     });
+ };
+
+ const getWalletBalance = () => {
+  // ToDo: Lookup how to move the X-API-Key to a .env file to keep it secret for when we push to Github
+     const headers = {
+       "X-Api-Key": "52cac212fc664da393ac45df991fdb84",
+     };
+     axios
+       .get("https://legend.lnbits.com/api/v1/wallet", { headers })
+       .then((res) => {
+         // Divide our balance by 1000 since it is denominated in millisats
+         setBalance(res.data.balance / 1000);
+       })
+       .catch((err) => console.log(err));
+   };
   
-  useEffect(() => {
-    getPrice();
-  }, []);
+   const getTransactions = () => {
+      // ToDo: Lookup how to move the X-API-Key to a .env file to keep it secret for when we push to Github
+      const headers = {
+        "X-Api-Key": "52cac212fc664da393ac45df991fdb84",
+      };
+      axios
+        .get("https://legend.lnbits.com/api/v1/payments", { headers })
+        .then((res) => {
+          setTransactions(res.data);
+        })
+        .catch((err) => console.log(err));
+    };
+   
+ // useEffect is a 'hook' or special function that will run code based on a trigger
+ // The brackets hold the trigger that determines when the code inside of useEffect will run
+ // Since it is empty [] that means this code will run once on page load
+ useEffect(() => {
+  getPrice();
+  getWalletBalance();
+  getTransactions();
+}, []);
 
-  return (
-    <div className='App'>
-      <h1>Christmas Sale 2023</h1>
-      <h2>Bitcoin Price Today!</h2>
-      <h3>${price}</h3>
-    </div>
-  );
+// Run these functions every 5 seconds after initial page load
+useEffect(() => {
+  const interval = setInterval(() => {
+    getPrice();
+    getWalletBalance();
+    getTransactions();
+  }, 5000);
+  return () => clearInterval(interval);
+}, []);
+
+ return (
+   <div className="App">
+     <header>
+       <h1>Plebbers Wallet Yo</h1>
+     </header>
+     {/* <Buttons /> */}
+     <div className="row">
+       <div className="balance-card">
+         <h2>Balance In Wee Sats</h2>
+         <p>{balance} sats</p>
+       </div>
+       <div className="balance-card">
+         <h2>Price of Corn</h2>
+         <p>${price}</p>
+       </div>
+     </div>
+     <div className="row">
+       <div className="row-item">
+         <Transactions transactions={transactions} />
+       </div>
+       <div className="row-item">{/* <Chart chartData={chartData} /> */}</div>
+     </div>
+     <footer>
+       <p>Made by plebs, for plebs to stack that corny shit.</p>
+     </footer>
+   </div>
+ );
 }
 
 export default App;
